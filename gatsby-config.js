3aa62a8e5ce892e0960e4e8c7f97eeb4fbeb23a1
alexander-rivaldy <alexander.rivaldy@thoughtworks.com>
2019-03-27 12:10:29 +1100

    [Alex] Change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `No Brakezzz Experience Report`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `No Brakezzz`,
+    description: `We progress without braking, we are No Brakezzz.`,
+    author: `Team No Brakezzz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
