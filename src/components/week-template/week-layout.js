5477680380a85fbf5aedfb635ae18ba2fb9eb27f
alexander-rivaldy <alexander.rivaldy@thoughtworks.com>
2019-03-27 14:45:48 +1100

    [Alex] refactor week panels

--- src/components/week-template/week-layout.js
+++ src/components/week-template/week-layout.js
@@ -6,6 +6,64 @@ import {ImageWithClass} from "../image";
 
 const section = "section";
 
+export const Weeks = [
+    {
+        weekNum: 0,
+        weekTitle: "I just haven't met you yet",
+        weekSummary: "The week No Brakerzzz arrived in Pune."
+    },
+    {
+        weekNum: 1,
+        weekTitle: "No Brakerzzz Assemble",
+        weekSummary: "At this very week, No Brakezzz was born!"
+    },
+    {
+        weekNum: 2,
+        weekTitle: "Accelerate all day every day",
+        weekSummary: "Time to break the pipeline I guess?"
+    },
+    {
+        weekNum: 3,
+        weekTitle: "The Halfway checkpoint",
+        weekSummary: "For some reason we have so many things happening this week"
+    },
+    {
+        weekNum: 4,
+        weekTitle: "The uh-oh moment",
+        weekSummary: "Nope. Just, Nope."
+    },
+    {
+        weekNum: 5,
+        weekTitle: "Final acceleration",
+        weekSummary: "The bitter-sweet end of ThoughtWorks University 64"
+    }
+];
+
+export const AllWeekPanels = () => {
+    let elements = [];
+
+    for(let weekNum = 0; weekNum < Weeks.length; weekNum++) {
+        elements.push( <WeekPanel week={Weeks[weekNum]}/> );
+    }
+
+    return elements;
+}
+
+
+const WeekPanel = ({week}) => (
+    <a href={"/week-" + week.weekNum}>
+        <div className="week-panel">
+            <h1>Week {week.weekNum}</h1>
+            <h4>{week.weekTitle}</h4>
+            <p>{week.weekSummary}</p>
+        </div>
+    </a>
+)
+
+WeekPanel.propTypes = {
+    week: PropTypes.node,
+}
+
 export const WeekBanner = ({ weekNum, weekTitle }) => (
 
     <div className="home-banner week-banner">
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,11 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import PropTypes from "prop-types"
 import ReactGA from 'react-ga';
+import {AllWeekPanels} from "../components/week-template/week-layout";
+
+
+
 
 const IndexPage = () => (
     <Layout>
@@ -13,38 +16,7 @@ const IndexPage = () => (
         <Banner />
 
         <div className="week-panels-container">
-            <WeekPanel weekNum={"0"} weekTitle={"I just haven't met you yet"}
-                weekDescription="
-                    The week No Brakerzzz arrived in Pune.
-                "
-
-            />
-
-            <WeekPanel weekNum={"1"} weekTitle={"No Brakerzzz Assemble!"}
-               weekDescription="
-                    At this very week, No Brakezzz was born!
-                "
-            />
-            <WeekPanel weekNum={"2"} weekTitle={"Accelerate all day every day"}
-               weekDescription="
-                   Time to break the pipeline I guess?
-               "
-            />
-            <WeekPanel weekNum={"3"} weekTitle={"The Halfway checkpoint"}
-               weekDescription="
-                   For some reason we have so many things happening this week.
-               "
-            />
-            <WeekPanel weekNum={"4"} weekTitle={"The uh-oh moment"}
-               weekDescription="
-                    Nope. Just, Nope.
-                "
-            />
-            <WeekPanel weekNum={"5"} weekTitle={"Final acceleration"}
-               weekDescription="
-                   The bitter-sweet end of ThoughtWorks University 64.
-               "
-            />
+            <AllWeekPanels/>
         </div>
 
     </Layout>
@@ -58,22 +30,8 @@ const Banner = () => (
 )
 
 
-const WeekPanel = ({weekNum, weekTitle, weekDescription}) => (
-    <a href={"/week-" + weekNum}>
-        <div className="week-panel">
-            <h1>Week {weekNum}</h1>
-            <h4>{weekTitle}</h4>
-            <p>{weekDescription}</p>
-        </div>
-    </a>
-
-)
 
 
-WeekPanel.propTypes = {
-    weekNum: PropTypes.string,
-    weekTitle: PropTypes.string,
-    weekDescription: PropTypes.string,
-}
+
 
 export default IndexPage
